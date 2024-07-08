8d511806c7e826afc5c8d618ab125d6af23e54d1
Gikuhi <richardgikm@gmail.com>
2024-07-08 13:02:41 +0300

    1st Commit

---
+++ src/content_option.js
@@ -0,0 +1,175 @@
+const logotext = "RICK";
+const meta = {
+    title: "Richard Gikuhi",
+    description: "I’m Richard Gikuhi computer scientist _ Full stack developer,currently working in Nairobi",
+};
+
+const introdata = {
+    title: "I’m Richard Gikuhi",
+    animated: {
+        first: "I create cool websites",
+        second: "I love photography & cinematography",
+        third: "My hobby is graphic design",
+    },
+    description: "Meet Richard Gikuhi, a dedicated and hardworking professional with a passion for graphic design and web development. With an unwavering commitment to excellence, he approaches each project with creativity, attention to detail, and a drive to exceed expectations. As a graphic designer, Gikuhi has a flair for blending artistic vision with strategic thinking to create visually stunning and impactful designs that resonate with audiences. In the realm of web development, he combines technical expertise with an eye for aesthetics to craft seamless, user-friendly digital experiences. Whether it's designing captivating visuals or building functional websites, Richard consistently strives for innovation and quality in all aspects of their work. With their strong work ethic and determination, you can trust that he will go above and beyond to bring your creative visions to life.",
+    // your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d", 
+    your_img_url: "images/my_portrait.jpeg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    // img: "public/images/my_portrait.jpeg",
+    aboutme: "I'm 22 years of age, Schooling at Kenyatta University. I am a passionate UI/UX designer with a keen eye for creating visually stunning and user-friendly interfaces. My goal is to blend creativity with functionality, ensuring that every design not only looks great but also enhances the overall user experience. With a background in graphic design and a strong understanding of human-centered design principles, I strive to create designs that are both aesthetically pleasing and intuitive to navigate. I am dedicated to staying updated on the latest design trends and leveraging technology to create impactful digital experiences. I am excited to share my work with you and look forward to the opportunity of collaborating on your next project.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Django",
+        value: 65,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Adobe Photoshop",
+        value: 85,
+    },
+    {
+        name: "Python",
+        value: 60,
+    },
+   
+    {
+        name: "Adobe AfterEffects",
+        value: 90,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I am dedicated to crafting captivating and user-friendly interfaces that elevate the digital experience.",
+    },
+    {
+        title: "Photography",
+        description: "In photography, I specialize in capturing memorable moments and compelling visual narratives through my lens.",
+    },
+    {
+        title: "Cinematography",
+        description: "My passion for cinematography allows me to translate stories into visually stunning and impactful motion pictures. With a keen eye for detail and a commitment to excellence, I strive to deliver exceptional results in each of these artistic disciplines. ",
+    },
+];
+
+const dataportfolio = [{
+        img: "images/coast-3.png",
+        description: "Mombasa Coast #1.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "richardgikm@.com",
+    YOUR_FONE: "(+254)717-577640",
+    // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Gikuhi",
+    instagram: "https://instagram.com/Gikuhii",
+    linkedin: "https://www.linkedin.com/in/richard-murimi-96173a229/",
+    twitter: "https://x.com/rickkkgikkks",
+    youtube: "https://youtube.com/@gikuhii?si=bFRSXyJ7kWsSBeVk",
+    // pinterest: "https://www.pinterest.com/gikuhiii/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
